a39a3519799db562a14833e7183b12957cc304a3
KML <kaungmyatlin.worklife@gmail.com>
2022-09-06 17:17:24 +0630

    update

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-function PostPage() {
+function PostDetailPage() {
     return (
-        <div>[id]</div>
+        <div>
+            
+        </div>
     )
 }
 
-export default PostPage
+export default PostDetailPage
