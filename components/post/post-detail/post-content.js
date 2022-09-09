82d3773789a20e8d7c3701453ad221aa883737ab
KML <kaungmyatlin.worklife@gmail.com>
2022-09-09 12:11:34 +0630

    update

--- components/post/post-detail/post-content.js
+++ components/post/post-detail/post-content.js
@@ -1,19 +1,20 @@
 import PostHeader from './post-header'
 import classes from './post-content.module.css'
 import ReactMarkDown from 'react-markdown'
-const DUMMY_POSTS = {
-        title: "Getting started with nextjs",
-        image: "getting-started-with-nextjs.png",
-        date: "2022-02-10",
-        slug: "getting-started-with-nextjs",
-        content: '# This is a first post'
-    }
-function PostContent() {
-    const imagePath= `/images/post/${DUMMY_POSTS.slug}/${DUMMY_POSTS.image}`
+// const DUMMY_POSTS = {
+//         title: "Getting started with nextjs",
+//         image: "getting-started-with-nextjs.png",
+//         date: "2022-02-10",
+//         slug: "getting-started-with-nextjs",
+//         content: '# This is a first post'
+//     }
+function PostContent(props) {
+    const {post} = props;
+    const imagePath= `/images/post/${post.slug}/${post.image}`
     return (
         <article className={classes.content}>
-            <PostHeader title={DUMMY_POSTS.title} image={imagePath}/>
-            <ReactMarkDown> {DUMMY_POSTS.content} </ReactMarkDown>
+            <PostHeader title={post.title} image={imagePath}/>
+            <ReactMarkDown> {post.content} </ReactMarkDown>
         </article>
     )
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,9 +1,29 @@
 import PostContent from '../../components/post/post-detail/post-content'
-
-function PostDetailPage() {
+import { getPostData, getPostFiles } from '../../lib/post-util'
+function PostDetailPage(props) {
     return (
-        <PostContent  />
+        <PostContent post={props.post}/>
     )
+}
+
+export function getStaticProps(context) {
+    const {params} = context;
+    const {slug} = params;
+    const postData = getPostData(slug);
+    return {
+        props: {
+            post: postData
+        },
+        revalidate: 600
+    }
+}
+export function getStaticPaths() {
+    const postFilesName = getPostFiles();
+    const slugs = postFilesName.map (fileN => fileN.replace(/\.md$/, ''));
+    return {
+        paths: slugs.map(slug => ({params: {slug: slug}})),
+        fallback: false
+    }
 }
 
 export default PostDetailPage
